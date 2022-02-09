33a04511c1a6214bb5824b7317bfb2a42b914cc8
Rebecca Rozmajzl <rebecca.rozmajzl@gmail.com>
2022-02-09 16:21:08 -0500

    commit tree 48c0a89c37e7a0a2df37263ad5c98c31d7e9cc54

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-const employee = {
-    name : 'Sam',
-    streetAddress : '11 Broadway',
-}
+// const employee = {
+//     name : 'Sam',
+//     streetAddress : '11 Broadway',
+// }
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployeeNonDes = {...employee};
-    newEmployeeNonDes[key] = value;
-    return newEmployeeNonDes;
-}
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newEmployeeNonDes = {...employee};
+//     newEmployeeNonDes[key] = value;
+//     return newEmployeeNonDes;
+// }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value
-    return employee;
-}
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value
+//     return employee;
+// }
 
-function deleteFromEmployeeByKey(employee, key) {
-    const newObj = Object.assign({}, employee, key);
-    delete newObj[key];
-    return newObj;
-}
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newObj = Object.assign({}, employee, key);
+//     delete newObj[key];
+//     return newObj;
+// }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key]
-    return employee;
-}
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key]
+//     return employee;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
